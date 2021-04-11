46902e7971420fca3c40ab31a874fdfac1cb8ae1
Mohammed <mohammedbassam272@gmail.com>
2021-04-11 14:24:39 +0300

    add slider

--- app/src/main/java/com/calculation/design/Loge_activity.java
+++ app/src/main/java/com/calculation/design/Loge_activity.java
@@ -8,6 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.asura.library.posters.Poster;
+import com.asura.library.posters.RawVideo;
+import com.asura.library.posters.RemoteImage;
+import com.asura.library.posters.DrawableImage;
+import com.asura.library.posters.RemoteVideo;
+import com.asura.library.views.PosterSlider;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Loge_activity extends AppCompatActivity {
 
     @Override
@@ -19,6 +29,14 @@ public class Loge_activity extends AppCompatActivity {
         final Button btnregistere = findViewById(R.id.btnregistere);
         final Button btnlogine = findViewById(R.id.btnlogine);
 
+        final PosterSlider posterSlider = findViewById(R.id.poster_slider);
+        List<Poster> posters=new ArrayList<>();
+
+        posters.add(new DrawableImage(R.drawable.illustration_log_in));
+        posters.add(new DrawableImage(R.drawable.illustration_log_in));
+        posters.add(new DrawableImage(R.drawable.illustration_log_in));
+        posters.add(new DrawableImage(R.drawable.illustration_log_in));
+        posterSlider.setPosters(posters);
 
         btnlogine.setOnClickListener(new View.OnClickListener() {
             @Override
